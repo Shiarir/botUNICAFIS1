79d060a5a570612c4420d54ac4f8e23f4405477b
Shiarir <shiarir40@hotmail.com>
2020-08-05 19:56:35 -0500

    Version 2.0
    Version 2.0

--- botUNICAFIS1-src/Bots/EchoBot.cs
+++ botUNICAFIS1-src/Bots/EchoBot.cs
@@ -19,7 +19,7 @@ namespace Microsoft.BotBuilderSamples.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hola bienvenidos soy el bot v2.0!";
+            var welcomeText = "Hola bienvenidos soy el bot v2.00! w";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
